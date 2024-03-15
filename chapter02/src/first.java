7f9a4beacde665f29134d9907b4a07ca29e20675
김경현 <127646410+GaengSSeu04@users.noreply.github.com>
2024-03-15 13:11:31 +0900

    Add files via upload

---
+++ chapter02/src/first.java
@@ -0,0 +1,17 @@
+
+/*
+ *  작성일 : 2024년 3월 8일
+ *  작성자 : 학과 학번 이름
+ *  설명 : 첫 번째 자바 프로그램
+ */
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+		// 'ln' 은 줄바꿈이다.
+		System.out.print("김경현입니다.");
+		System.out.println("반갑습니다." + "첫 번째 자바 프로그램입니다.");
+		// +는 연결 연산자이다.
+	}
+
+}
